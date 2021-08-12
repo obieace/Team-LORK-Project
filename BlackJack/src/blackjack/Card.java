a5670201367feb7353ddd845827c53b85fde4b89
dabro <dabro@DESKTOP-3V8HISK>
2021-08-12 14:51:01 -0400

    Can draw a card now

--- BlackJack/src/blackjack/Card.java
+++ BlackJack/src/blackjack/Card.java
@@ -7,8 +7,6 @@ package blackjack;
 public class Card { // reusing to print UNO (color+rank) card 
     //Loose coupling,High cohesion,delegation and encapsulation - USED to great software 
     // enums are predefined contants and special datatype
-        public enum Suit{DIAMONDS,SPADES,CLUBS,HEARTS};
-        public enum Value{ACE,TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,TEN,JACK,KING,QUEEN};//ADD JOKER AND SEE THE EXECUTION,CODE IS EASY FOR UPDATION NOW 
         private final Suit suit; //varaibles are encapsulated
         private final Value value;
         
@@ -17,11 +15,11 @@ public class Card { // reusing to print UNO (color+rank) card
            suit =s;
            value= gVal;
         }
+        
 	public Value getValue() {
 		return this.value;
 	}
 
-	
 	public Suit getSuit() {
 		return this.suit;
         }
